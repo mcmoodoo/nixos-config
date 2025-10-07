8c40fd03ad8769dd688bc2713dcc8dfae89a7fd1
mcmoodoo <rashid@mcmoodoo.com>
2025-10-06 21:48:35 -0500

    set brave as default browser via a session variable. Not sure of its effects.

--- configuration.nix
+++ configuration.nix
@@ -55,6 +55,10 @@
 
   programs.firefox.enable = true;
 
+  environment.sessionVariables = {
+    BROWSER = "brave";
+  };
+
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [

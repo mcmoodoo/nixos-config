fc086d6336ed3392a425dcb2a860253a5f185587
mcmoodoo <rashid@mcmoodoo.com>
2025-10-26 07:49:28 -0400

    dynamic linker added for loading shared libraries

--- configuration.nix
+++ configuration.nix
@@ -161,6 +161,8 @@
     pkgs.nerd-fonts.hack
   ];
 
+  programs.nix-ld.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are started in user sessions.
   programs.mtr.enable = true;
   programs.gnupg.agent = {
